ba8d0725dd1202bea22f7cb7b98d03e22c2415b4
Ilan Nestor Angeles Rodriguez <ilananagelesrodriguez@gmail.com>
2023-11-23 20:27:18 -0500

    feat: añadir stopPropagation a los eventos onClick

--- src/components/Events/components/EventItem/index.jsx
+++ src/components/Events/components/EventItem/index.jsx
@@ -1,10 +1,16 @@
 export const EventItem = ({ info, id, name, image, onEventClick }) => {
+
+    const handleSeeMoreClick = (e) => {
+        e.stopPropagation();
+        onEventClick(id);
+    }
+
     return (
-        <div>
+        <div onClick={() => console.log('padre clickeado')}>
             <img src={image} alt={name} width={200}/>
             <h4>{name}</h4>
             <p>{info}</p>
-            <button onClick={ () => onEventClick(id) }>Ver más</button>
+            <button onClick={ handleSeeMoreClick }>Ver más</button>
         </div>
     );
 }
